5b233e06fa23ae7db3e410630a55002ea5d04077
Julian Richardson <jrichar3@yahoo.com>
2022-03-22 22:26:57 -0400

    added console logs for correct answers as well as incorrect answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var timerEl = document.getElementById("timer");
 var startEl = document.getElementById("start-button");
 
 var currentIndex = 0;
-
+// this is the list of questions and their corresponding answers
 var questionsArray = [
     {
         text: "how many cylinders does an N54 engine run on?",
@@ -59,22 +59,17 @@ var questionsArray = [
     }
 ]
 
-    
 
-
-
-// Sets first pTags to have a font-size of 40px
-// pTags[0].setAttribute("style", "font-size: 40px;");
-
+// this indicates the amount of time for questions
 var secondsLeft = 50;
-
+// used to set the timer for countdown
 function setTime() {
   // Sets interval in variable
   var timerInterval = setInterval(function() {
     secondsLeft--;
     timerEl.textContent = secondsLeft;
-
-    if(secondsLeft === 0) {
+// when timer hits 0 executes next action
+    if(secondsLeft <= 0) {
       // Stops execution of action at set interval
       clearInterval(timerInterval);
       // Calls function to create and append image
@@ -84,52 +79,83 @@ function setTime() {
   }, 1000);
 }
 
-
+// this function initiates the quiz
 function startQuiz(){
-    // var newElement= "<input type='time' name='count'>";
-    // document.body.innerHTML=newElement;
     setTime()
     replacePage()
     displayQuestion()
     }
-
+// when start is click,the quiz begins
 startEl.addEventListener("click", startQuiz)
-
+// this function removes the title and paragrapgh from page, start quiz
 function replacePage() {
     var questionContainer=document.getElementById("questionContainer");
     questionContainer.style.display = "block";
     var startContainer=document.getElementById("startContainer");
     startContainer.style.display = "none";
+    var highscoreContainer=document.getElementById("highscoreContainer");
+    highscoreContainer.style.display = "none";
 }
-
+// this function controls the display of questions
 function displayQuestion () {
     var quizQuestion = document.getElementById("quizQuestion");
     quizQuestion.textContent = questionsArray[currentIndex].text;
-
+// this controls the 1st choice answer
     var choice1 = document.getElementById("choice1");
     choice1.textContent = questionsArray[currentIndex].options[0];
+    choice1.setAttribute("class", questionsArray[currentIndex].options[0])
     choice1.addEventListener("click", nextQuestion)
-
+// this controls the 2nd choice answer
     var choice2 = document.getElementById("choice2");
     choice2.textContent = questionsArray[currentIndex].options[1];
+    choice2.setAttribute("class", questionsArray[currentIndex].options[1])
     choice2.addEventListener("click", nextQuestion)
-    
-
+// this controls the 3rd choice answer
     var choice3 = document.getElementById("choice3");
     choice3.textContent = questionsArray[currentIndex].options[2];
+    choice3.setAttribute("class", questionsArray[currentIndex].options[2])
     choice3.addEventListener("click", nextQuestion)
-    
-
+// this controls the 4th choice answer
     var choice4 = document.getElementById("choice4");
     choice4.textContent = questionsArray[currentIndex].options[3];
+    choice4.setAttribute("class", questionsArray[currentIndex].options[3])
     choice4.addEventListener("click", nextQuestion)
     
 
 }
-
-function nextQuestion () {
+// this is for logging users answer in comparison to correct answer
+function nextQuestion (event) {
+    console.log(event.target.textContent)
+var userAnswer = event.target.textContent;
+// console.log(userAnswer.getAttribute("class"));
+if (userAnswer === questionsArray[currentIndex].correct) {
 
     currentIndex++;
     displayQuestion()
-
+} else {
+    secondsLeft = secondsLeft -15;
+    currentIndex++;
+    displayQuestion()
 }
+    
+clearInterval(timeinterval)
+}
+// timerrrrr!!!!!
+// function showHighscore (event) {
+//     var highscoreContainer=document.getElementById("highscoreContainer");
+//     highscoreContainer.style.display = "block";
+//     var startContainer=document.getElementById("startContainer");
+//     startContainer.style.display = "none";
+//     var questionContainer=document.getElementById("questionContainer");
+//     questionContainer.style.display = "none";
+
+// }
+// }
+// }
+
+// function displayHighscore () {
+
+//     var yourScore = document.getElementById("yourScore");
+// //     yourScore.textContent = 
+
+// }
